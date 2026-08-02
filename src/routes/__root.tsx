ec12075ab4631347a036047c7672149540aaa2fe
Firoz Khan <manorhub533@gmail.com>
2026-08-02 22:27:33 +0530

    feat: add vector SVG favicon and update root HTML icon links

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -97,7 +97,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.png", type: "image/png" },
+      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
+      { rel: "alternate icon", href: "/favicon.png", type: "image/png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
